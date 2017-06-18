0dea9db0d4848762f87375b726c46c9e426aae67
birdy90 <gob@live.ru>
2017-06-18 11:34:31 +0300

    * socket io - finished

--- SocketIO/index.js
+++ SocketIO/index.js
@@ -24,34 +24,52 @@ server.on('request', (req, res) => {
 });
 
 io.on('connection', (client) => {
-    console.log(`new client ${client.id}`);
+  console.log(`new client ${client.id}`);
+  client.emit('connection');
 
-    client.on('login', (data) => {
-        const loggedUser = users.find((item) => item.id === client.id );
-        if (loggedUser !== undefined) return;
+  client.on('login', (loginData) => {
+    const nameRegex = new RegExp(/^[a-zа-я\d]+$/, 'i');
+    const loggedUser = users.find((item) => item.id === client.id || (item.room === loginData.room && item.name === loginData.name))
+    if (loggedUser !== undefined || !nameRegex.test(loginData.name)) return;
 
-        console.log(`${client.id} logged in as ${data.name}`);
-        users.push({id: client.id, name: data.name});
-        client.emit('system', {message: 'Вы присоединились'});
-        client.broadcast.emit('system', {message: `Присоединился пользователь ${data.name}`});
-        io.emit('system', {message: `Теперь в чате ${users.length} пользователей`});
-
-        client.on('new-message', (data) => {
-            console.log('new message');
-            console.log(data);
-            client.broadcast.emit(data);
-        });
-        client.on('disconnect', () => {
-            console.log(`${client.id} disconnected`);
-
-            const left = users.find((item) => item.id !== client.id );
-            const index = users.indexOf(left);
-            users.splice(index, 1);
-            io.emit('system', {message: `Пользователь ${left.name} вышел из чата`});
-            io.emit('system', {message: `Теперь в чате ${users.length} пользователей`});
-        });
+    console.log(`${client.id} logged in as ${loginData.name}`);
+    const newUser = {
+      id: client.id,
+      name: loginData.name,
+      socket: client,
+      room: loginData.room
+    };
+    users.push(newUser);
+    findReceivers(users, newUser).forEach((item) => {
+      item.socket.emit('system', {message: `Присоединился пользователь ${loginData.name}`});
+      item.socket.emit('system', {message: `Теперь в чате ${users.map(item => item.room === loginData.room).length} пользователей`});
     });
+    client.emit('logged');
+    client.emit('system', {message: `Вы присоединились. Комната: ${loginData.room}`});
+
+    client.on('new-message', (data) => {
+      const sender = findUser(client);
+      data.username = sender.name;
+      findReceivers(users, sender).forEach((item) => {
+        item.socket.emit('new-message', data);
+      });
+    });
+    client.on('disconnect', () => {
+      console.log(`${client.id} disconnected`);
+      const sender = findUser(client);
+      if (sender === undefined) return;
+      const index = users.indexOf(sender);
+      users.splice(index, 1);
+      findReceivers(users, sender).forEach((item) => {
+        item.socket.emit('system', {message: `Пользователь ${sender.name} вышел из чата`});
+        item.socket.emit('system', {message: `Теперь в чате ${users.length} пользователей`});
+      });
+    });
+  });
 });
+
+const findUser = (client) => users.find((item) => item.id === client.id);
+const findReceivers = (users, sender) => users.filter((item) => item.room === sender.room && item.id !== sender.id);
 
 server.listen(3000);
 ioServer.listen(1234);
