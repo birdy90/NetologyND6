d9061523c009b1aea75129d6d5b01cc538b013cc
birdy90 <gob@live.ru>
2017-07-23 15:50:50 +0300

    * protractor - продолжение

--- Protractor/app/src/Menu/MenuCtrl.js
+++ Protractor/app/src/Menu/MenuCtrl.js
@@ -4,16 +4,18 @@ angular
   .component('menuBlock', {
     controller: function($scope, $element, $attrs) {
       this.activeMenu = '';
-
       this.menus = [
         {url: '', title: 'Главная'},
         {url: 'pokemons', title: 'Покемоны'},
         {url: 'create', title: 'Добавить нового'},
-      ]
+        {url: 'myaccount', title: 'Аккаунт'},
+      ];
+      this.click = (e) => {
+        this.activeMenu = e.target.href.split('#!/')[1];
+      }
     },
+    controllerAs: '$ctrl',
     templateUrl: './src/Menu/Menu.html',
     bindings: {
-      activeMenu: '=',
-      menus: '='
     }
   });
