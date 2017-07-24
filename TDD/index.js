568a23b8fda848e74209cde8c82ba413d03226f9
gbederdinov <gbederdinov@matvil.ru>
2017-07-24 16:29:35 +0300

    * tdd - 4

--- TDD/index.js
+++ TDD/index.js
@@ -2,12 +2,16 @@ class Calculator
 {
   static add(numbers) {
     numbers = numbers || '';
-    if (numbers.match(/[,\n]{2,}/)) {
-      throw new Error('Invalid dividers');
-    }
 
+    const divider = Utils.getDivider(numbers) || new Divider('(?:,|\n)', numbers);
+    const regexp = new RegExp(`${divider.divider}`);
+    const invalidRegexp = new RegExp(`${divider.divider}{2}`);
+
+    if (divider.numbers.match(invalidRegexp))
+      throw new Error('Invalid numbers');
     let sum = 0;
-    const parts = numbers.split(/[,\n]/).forEach((item) => {
+
+    const parts = divider.numbers.split(regexp).forEach((item) => {
       sum += Utils.handleParam(item);
     });
 
@@ -29,7 +33,28 @@ class Utils {
         str = 2;
       }
     }
-    return str;
+    return parseInt(str);
+  }
+
+  static getDivider(str) {
+    let hasDivider = str.match(/\/\/\<.?\>\n/);
+    let matches = str.match(/(\/\/\<([^\[\]\(\)\|\/\d])\>\n)?(.*)/);
+    if (hasDivider) {
+      if (!matches || matches.length === 1 || matches[2] === undefined) {
+        throw new Error('Invalid custom divider');
+      } else {
+        return new Divider(`(?:${matches[2]})`, matches[3]);
+      }
+    } else {
+      return undefined;
+    }
+  }
+}
+
+class Divider {
+  constructor(divider, string) {
+    this.divider = divider;
+    this.numbers = string;
   }
 }
 
--- TDD/test/index.js
+++ TDD/test/index.js
@@ -4,67 +4,101 @@ const calc = require('../index.js').calculator;
 const utils = require('../index.js').utils;
 
 describe('calculator', () => {
-  describe('parameters', () => {
-    describe('utils', () => {
-      it('checkEmpty empty', () => {
-        expect(utils.handleParam()).eql(0);
-      });
-      it('checkEmpty empty', () => {
-        expect(utils.handleParam('')).eql(0);
-      });
-      it('checkEmpty smaller', () => {
-        expect(utils.handleParam('-2')).eql(0);
-      });
-      it('checkEmpty bigger', () => {
-        expect(utils.handleParam('5')).eql(2);
-      });
-      it('checkEmpty common', () => {
-        expect(utils.handleParam('1')).eql(1);
-      });
+  describe('utils', () => {
+    it('checkEmpty empty', () => {
+      expect(utils.handleParam()).equal(0);
+    });
+    it('checkEmpty empty', () => {
+      expect(utils.handleParam('')).equal(0);
+    });
+    it('checkEmpty smaller', () => {
+      expect(utils.handleParam('-2')).equal(0);
+    });
+    it('checkEmpty bigger', () => {
+      expect(utils.handleParam('5')).equal(2);
+    });
+    it('checkEmpty common', () => {
+      expect(utils.handleParam('1')).equal(1);
     });
 
-    describe('main 1', () => {
-      it('empty parameters', () => {
-        expect(calc.add()).to.be.eql(0);
-      });
-      it('single blank parameter', () => {
-        expect(calc.add('')).to.be.eql(0);
-      });
-      it('single common parameter', () => {
-        expect(calc.add('1')).to.be.eql(1);
-      });
-      it('double parameters, first blank', () => {
-        expect(calc.add(',1')).to.be.eql(1);
-      });
-      it('double parameters, second blank', () => {
-        expect(calc.add('2,')).to.be.eql(2);
-      });
-      it('double parameters', () => {
-        expect(calc.add('2,2')).to.be.eql(4);
-      });
-      it('bigger parameters', () => {
-        expect(calc.add('3,1')).to.be.eql(3);
-      });
+    it('check divider no divider', () => {
+      expect(() => utils.getDivider('//<>\n1').divider).to.throw();
     });
-
-    describe('main 2', () => {
-      it('three parameters', () => {
-        // expect(calc.add('1,,4')).to.be.eql(3);
-        expect(() => calc.add('1,\n')).to.throw();
-      });
-      it('five parameters', () => {
-        expect(calc.add('1,2,3,4,')).to.be.eql(7);
-      });
+    it('check divider with number', () => {
+      expect(() => utils.getDivider('//<3>\n1').divider).to.throw();
     });
+    it('check divider ]', () => {
+      expect(() => utils.getDivider('//<]>\n1!2').divider).to.throw();
+    });
+    it('check divider incorrect', () => {
+      expect(utils.getDivider('//<;>\n1!2').divider).to.be.equal('(?:;)');
+    });
+    it('check divider correct;', () => {
+      expect(utils.getDivider('//<!>\n1!2').divider).to.be.equal('(?:!)');
+    });
+    it('check divider second part;', () => {
+      expect(utils.getDivider('//<!>\n1!2').numbers).to.be.equal('1!2');
+    });
+  });
 
-    describe('main 3', () => {
-      it('two dividers allowed', () => {
-        expect(calc.add('1\n2,3')).to.be.eql(5); // у нас же разрешены только 0, 1 и 2
-      });
-      it('two dividers disallowed', () => {
-        // expect(calc.add('1,\n')).to.be.eql(1); // вообще, должно быть разрешено, ведь пустой символ интерпретируется как 0
-        expect(() => calc.add('1,\n')).to.throw('Invalid dividers');
-      });
+  describe('main 1', () => {
+    it('empty parameters', () => {
+      expect(calc.add()).to.be.equal(0);
+    });
+    it('single blank parameter', () => {
+      expect(calc.add('')).to.be.equal(0);
+    });
+    it('single common parameter', () => {
+      expect(calc.add('1')).to.be.equal(1);
+    });
+    it('double parameters, first blank', () => {
+      expect(calc.add(',1')).to.be.equal(1);
+    });
+    it('double parameters, second blank', () => {
+      expect(calc.add('2,')).to.be.equal(2);
+    });
+    it('double parameters', () => {
+      expect(calc.add('2,2')).to.be.equal(4);
+    });
+    it('bigger parameters', () => {
+      expect(calc.add('3,1')).to.be.equal(3);
+    });
+  });
+
+  describe('main 2', () => {
+    it('three parameters', () => {
+      expect(() => calc.add('1,\n')).to.throw();
+    });
+    it('five parameters', () => {
+      expect(calc.add('1,2,3,4,')).to.be.equal(7);
+    });
+  });
+
+  describe('main 3', () => {
+    it('two dividers allowed', () => {
+      expect(calc.add('1\n2,3')).to.be.equal(5); // у нас же разрешены только 0, 1 и 2
+    });
+    it('two dividers disallowed', () => {
+      // expect(calc.add('1,\n')).to.be.equal(1); // вообще, должно быть разрешено, ведь пустой символ интерпретируется как 0
+      expect(() => calc.add('1,\n')).to.throw('Invalid numbers');
+    });
+  });
+
+  describe('main 4', () => {
+    it('check divider no divider', () => {
+      expect(() => calc.add('//<>\n1')).to.throw();
+    });
+    it('check divider invalid format', () => {
+      expect(calc.add('/>\n1')).to.be.NaN;
+    });
+    it('check divider with number', () => {
+      expect(() => calc.add('//<3>\n3')).to.throw();
+    });
+    it('check divider incorrect', () => {
+      expect(calc.add('//<;>\n1!2')).to.be.equal(1);
+    });
+    it('check divider correct;', () => {
+      expect(calc.add('//<!>\n1!2')).to.be.equal(3);
     });
   });
 });
